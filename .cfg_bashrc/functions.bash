a1c3c424936a290c92b431284bc04e0238aed5bb
RoyVorster <royvorster@gmail.com>
2018-08-29 11:44:43 +0200

    Add optirun function

--- .cfg_bashrc/functions.bash
+++ .cfg_bashrc/functions.bash
@@ -1,3 +1,6 @@
 # Functions for getting into certain folders
 function dut { cd ~/Documents/DUT/$1; }
 function proj { cd ~/Document/Projects/$1; }
+
+# For bumblebee
+function opt { optirun $1; exit; }
