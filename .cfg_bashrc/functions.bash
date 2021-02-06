b37f6c616b1fe47229047f0500d0a195417f09d7
Roy Vorster <royvorster@gmail.com>
2021-02-06 11:16:00 +0100

    Updates

--- .cfg_bashrc/functions.bash
+++
@@ -1,2 +0,0 @@
-# Link python script to $HOME/.bin
-function link_bin { ln -s $1 $HOME/.bin/$2; }
--- .cfg_zshrc/aliases.zsh
+++ .cfg_zshrc/aliases.zsh
@@ -1,9 +1,12 @@
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+
 alias cls='clear'
 
 alias sus='systemctl suspend; exit'
 alias hib='systemctl hibernate -i; exit'
 
-alias matlab='$HOME/MATLAB/2018a/bin/matlab -softwareopengl'
+alias matlab='$HOME/.matlab/matlab/bin/matlab -softwareopengl'
 
 alias stm='$HOME/SystemWorkbench/eclipse'
 alias stmcube='$HOME/STM32CubeMX/STM32CubeMX'
@@ -15,3 +18,13 @@ alias rsync='rsync -av'
 alias eps-export='. ~/.tmp/esp-idf/export.sh'
 
 alias nb='jupyter notebook --no-browser --port 8081'
+
+# Git aliases
+alias gl='git pull'
+alias ga='git add'
+alias gp='git push'
+alias gd='git diff'
+alias gc='git commit'
+alias gco='git checkout'
+alias gst='git status'
+alias grt='cd $(git rev-parse --show-toplevel)'
