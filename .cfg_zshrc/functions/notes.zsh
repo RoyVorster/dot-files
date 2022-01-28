ba6d22fc7e7e22192b56bcd7a159405b2f064086
Roy Vorster <royvorster@gmail.com>
2022-01-28 14:14:41 +0100

    Note taking update

--- .cfg_zshrc/functions/notes.zsh
+++ .cfg_zshrc/functions/notes.zsh
@@ -1,9 +1,18 @@
 #!/bin/zsh
 
-d='$HOME/fun/notes/'
-alias note-dir='cd $d'
+export d="$HOME/notes";
 
-function new-note-dir {
-    newdir=$(date +'%Y-%m-%d')
-    mkcd "$d/$newdir"
+# Edit note
+function ne {
+    f=$(find $d -depth -mindepth 1 | fzf --height 30% -m --reverse --query "$1")
+    if [[ $f ]]; then nvim $f; fi
 }
+
+# New note
+function nn {
+    if [[ $# -eq 0 ]]; then echo 'Name required: nn [NAME] [OPT:DIR]' && return; fi
+    if [[ $2 -ne 0 ]]; then d=$d/$2; fi
+
+    nvim $d/$(date +'%Y-%m-%d')-$1.md
+}
+
